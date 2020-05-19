8e6ab18f3eb071f31390362d9559038148d94824
diegolina <59884258+diegolina@users.noreply.github.com>
2020-05-18 22:42:15 -0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://diego:5wAx6zkvELtp8T31@cluster0-wqpkw.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 };
 
 process.env.URLDB = urlDB;
