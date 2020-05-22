f9bbea157162a699b370903ac173302153ffd534
diegolina <59884258+diegolina@users.noreply.github.com>
2020-05-22 19:52:49 -0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,12 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+/////VTO DEL TOKEN///////////////////////////////
+process.env.CADUCIDAD_TOKEN = (60 * 60 * 24 * 30);
+
+/////SEED DE AUTENTICACION///////////////////////
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
 /////BASE DE DATOS///////////////////////////////
 
 let urlDB;
@@ -14,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://diego:5wAx6zkvELtp8T31@cluster0-wqpkw.mongodb.net/cafe';
 };
 
 process.env.URLDB = urlDB;
